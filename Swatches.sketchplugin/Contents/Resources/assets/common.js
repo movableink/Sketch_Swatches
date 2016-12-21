15bffebbcaedd79993e88b17f6a58287d83ec38f
Ashung (iMac) <ashung.hung@gmail.com>
2016-12-21 18:57:16 +0800

    (WIP)

--- Swatches.sketchplugin/Contents/Resources/assets/common.js
+++ Swatches.sketchplugin/Contents/Resources/assets/common.js
@@ -1,43 +1,9 @@
-
-// for (var i = 0; i < swatches.length; i++) {
-//     loadScript(swatches[i].script);
-// }
-
-$(document).ready(function() {
-    // Load Menu
-    var menu = $('<ul class="menulist"></ul>');
-    for (var i = 0; i < swatches.length; i++) {
-        var title = swatches[i].title;
-        var handler = swatches[i].handler;
-        var liCode = '<li><button onclick="' + handler + '(\'' + title + '\');window.location.hash=\'' + handler + '\'">' + title + '</button></li>';
-        menu.append(liCode);
-    }
-    $("#menu").append(menu);
-
-    // Menu
-    $("#menuToggle").click(function() {
-        $("#menu").css("display", "block");
-        $("#menu").animate({
-            "height" : $("body").height()
-        }, 200);
-    });
-    $("#menuToggle-close").click(function(){
-        $("#menu").animate({
-            "height" : "0"
-        }, 200);
-    });
-    $(".menulist button").click(function() {
-        $("#menu").animate({
-            "height" : "0"
-        }, 200);
-    });
-});
-
-function loadScript(url) {
-    var script = document.createElement("script");
-    script.src = url;
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
+/*==========================================================
+    Swatches Plugin for Sketch
+    Author: Ashung Hung
+    Homepage: https://github.com/Ashung/Sketch_Swatches
+    License: CC-BY 4.0
+==========================================================*/
 
 function removeSwatches() {
     $("#swatches").empty();
