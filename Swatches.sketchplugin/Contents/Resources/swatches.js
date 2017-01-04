5bbc6041c17baf9604fd1043788838ba05bb3a51
Ashung (iMac) <ashung.hung@gmail.com>
2017-01-04 15:45:31 +0800

    (WIP) Add pantone colors.

---
+++ Swatches.sketchplugin/Contents/Resources/swatches.js
@@ -0,0 +1,62 @@
+var swatches = [
+    {
+        "title" : "Material Design",
+        "handler" : "init_material_design_colors"
+    },
+    {
+        "title" : "Open Color",
+        "handler" : "init_open_color"
+    },
+    {
+        "title" : "Web Colors",
+        "handler" : "init_web_colors"
+    },
+    {
+        "title" : "Name Colors",
+        "handler" : "init_name_colors"
+    },
+    {
+        "title" : "Pantone Formula Guide Solid Coated",
+        "handler" : "init_pantone_formula_guide_solid_coated"
+    },
+    {
+        "title" : "Pantone Formula Guide Solid Uncoated",
+        "handler" : "init_pantone_formula_guide_solid_uncoated"
+    },
+    {
+        "title" : "Pantone Formula Guide Solid Coated 112 New Colors",
+        "handler" : "init_pantone_formula_guide_solid_coated_new_colors"
+    },
+    {
+        "title" : "Pantone Formula Guide Solid Uncoated 112 New Colors",
+        "handler" : "init_pantone_formula_guide_solid_uncoated_new_colors"
+    },
+    {
+        "title" : "Pantone Color Bridge Coated",
+        "handler" : "init_pantone_color_bridge_coated"
+    },
+    {
+        "title" : "Pantone Color Bridge Uncoated",
+        "handler" : "init_pantone_color_bridge_uncoated"
+    },
+    {
+        "title" : "RAL Color",
+        "handler" : "init_ral_classic_colors"
+    },
+    {
+        "title" : "TOYO Color",
+        "handler" : "init_toyo_color"
+    },
+    {
+        "title" : "DIC Color",
+        "handler" : "init_dic_color"
+    },
+    {
+        "title" : "中国の伝統色",
+        "handler" : "init_traditinal_chinese_colors"
+    },
+    {
+        "title" : "日本の伝統色",
+        "handler" : "init_traditinal_japanese_colors"
+    }
+];
